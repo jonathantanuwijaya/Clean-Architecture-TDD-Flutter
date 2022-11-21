230e78a6d9fa37b0d6a01868b27c3161f349fc2a
Jonathan Tanuwijaya <jonathantw@andalsoftware.com>
2022-11-21 19:21:07 +0700

    create input converter and test

---
+++ lib/core/utils/input_converter.dart
@@ -0,0 +1,19 @@
+import 'package:clean_arch_tdd/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+
+class InputConverter {
+  Either<Failure, int>? stringToUnsignedInteger(String str) {
+    try{
+      final integer = int.parse(str);
+      if (integer < 0) throw const FormatException('Negative number');
+      return right(integer);
+    } on FormatException {
+      return left(InvalidInputFailure());
+    }
+  }
+}
+
+class InvalidInputFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
